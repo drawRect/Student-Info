dba4b1e6164fa6435da0b8200e7e329953f39b65
Boominadha Prakash M <boominadha.pm@sonata-software.com>
2018-06-13 10:16:29 +0530

    Removed unwanted lines from the testcases

--- StudentsUnitTests/JsonLoaderTests.swift
+++ StudentsUnitTests/JsonLoaderTests.swift
@@ -7,17 +7,20 @@
 //
 
 import XCTest
+@testable import Students
 
 class JsonLoaderTests: XCTestCase {
     
-    private var bundle:Bundle?
+    private lazy var bundle:Bundle = {
+        let b = Bundle(for: type(of: self))
+        return b
+    }()
     private let invalidJSONFileName = "invalid.json"
     private let invalidFile = "invalidTextFile.txt"
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        bundle = Bundle(for: type(of: self))
     }
     
     override func tearDown() {
@@ -26,37 +29,29 @@ class JsonLoaderTests: XCTestCase {
     }
     
     func testJSONLoaderForValidJson() {
-        let expectation = XCTestExpectation(description: "Test JSON file")
         let resource = Resource(name: Constants.studJSONFileName, A: Students.self)
         let stud = try? JSONLoader.loadMockFile(resource)
         XCTAssertNotNil(stud, "stud should not be nil")
-        expectation.fulfill()
-        self.wait(for: [expectation], timeout: 10.0)
     }
     
     func testJSONLoaderForInvalidJSON() {
-        let expectation = XCTestExpectation(description: "Test Invalid JSON file")
         let resource = Resource(name: invalidJSONFileName, A: Professors.self)
         do {
-            let _ = try JSONLoader.loadMockFile(resource, bundle: bundle!)
+            let _ = try JSONLoader.loadMockFile(resource, bundle: bundle)
         }catch let error {
             if let e = error as? MockLoaderError {
                 XCTAssertEqual(e.localizedDescription, "\(invalidJSONFileName) has Invalid JSON")
-                expectation.fulfill()
             }
         }
-        self.wait(for: [expectation], timeout: 10.0)
     }
     
     func testJSONLoaderForIncorrectFile() {
-        let expectation = XCTestExpectation(description: "Test JSON Loader with incorrect file type")
         let resource = Resource(name: invalidFile, A: Students.self)
         do {
-            let _ = try JSONLoader.loadMockFile(resource, bundle: bundle!)
+            let _ = try JSONLoader.loadMockFile(resource, bundle: bundle)
         }catch let error {
             if let e = error as? MockLoaderError {
                 XCTAssertEqual(e.localizedDescription, "\(invalidFile) FileName is incorrect")
-                expectation.fulfill()
             }
         }
     }
