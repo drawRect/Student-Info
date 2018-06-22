1b0cbff616ae603052035db167aa93b32c0ebf65
ranjit <ranjithkumar2010a@live.com>
2018-06-23 02:05:24 +0530

    Generic TableViewHelper implemented for Professors Module

--- Students/Professor/ProfessorListViewModel.swift
+++ Students/Professor/ProfessorListViewModel.swift
@@ -10,47 +10,47 @@ import Foundation
 import UIKit
 
 class ProfessorListViewModel: NSObject {
-    var professors: Professors?
+    let professors: Professors
+    init(_ professors: Professors) {
+        self.professors = professors
+    }
     var sectionedProfessors: [SexualType: [Professor]] {
-        if let professors = professors?.professors {
-            return Dictionary(grouping: professors,by:{$0.sexualType})
-        }
-        return [:]
+        return Dictionary(grouping: professors.professors,by:{$0.sexualType})
     }
     
-    func getDataSource(completion: @escaping(_ result: Result<Professors>)->())  {
-        do {
-            let resource = Resource(name: Constants.profJSONFileName, A: Professors.self)
-            let professors = try JSONLoader.loadMockFile(resource)
-            completion(Result.success(professors))
-        } catch (let e){
-            completion(Result.failure(e))
-        }
+    private func getSexuality(for section: Int) -> SexualType {
+        return Array(sectionedProfessors.keys)[section]
     }
 }
 
 //MARK: - Extension|ProfessorListViewModel
-extension ProfessorListViewModel: UITableViewDelegate,UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+extension ProfessorListViewModel: TVDataSourceConfigurable {
+    typealias T = Professor
+    var array: [Professor] {
+        return professors.professors
+    }
+
+    func numberOfSections() -> Int {
         return sectionedProfessors.keys.count
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sexuality = Array(sectionedProfessors.keys)[section]
+    func numberOfRows(in section: Int) -> Int {
+        let sexuality = getSexuality(for: section)
         return sectionedProfessors[sexuality]?.count ?? 0
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = ProfessorInfoCell(style: .subtitle, reuseIdentifier: ProfessorInfoCell.reuseIdentifier)
-        let sexuality = Array(sectionedProfessors.keys)[indexPath.section]
-        let professor = (sectionedProfessors[sexuality]?[indexPath.row])!
-        cell.populateCell(with: professor)
-        return cell
+    func cellForRow(at indexPath: IndexPath) -> T? {
+        let sexuality = getSexuality(for: indexPath.section)
+        return (sectionedProfessors[sexuality]?[indexPath.row])
     }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return Array(sectionedProfessors.keys)[section].rawValue
+
+    func titleForHeader(in section: Int) -> String? {
+        return getSexuality(for: section).rawValue
     }
-    
+}
+
+extension ProfessorListViewModel: UITableViewDelegate {
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int){
         let header = view as! UITableViewHeaderFooterView
         header.textLabel?.textColor = Constants.headerColor
     }
 }
+
